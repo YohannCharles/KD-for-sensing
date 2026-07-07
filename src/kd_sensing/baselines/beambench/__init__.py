d7da099beaf51b8f1cf7fb3333da12311766ac79
Yohann <13758654108@163.com>
2026-07-07 19:51:47 +0800

    2.4.5 清理支线

--- src/kd_sensing/baselines/beambench/__init__.py
+++
@@ -1,4 +0,0 @@
-"""BeamBench baseline package.
-
-Import concrete owners such as ``image_ae_gps_training`` directly.
-"""
--- src/kd_sensing/baselines/rmbp_mm/__init__.py
+++
@@ -1,1 +0,0 @@
-"""RMBP-MM package marker. Import workflow helpers from ``.workflow``."""
